2c7989c4639c0837c7f2afa126a155418ca8a53b
mis1nput <161495409+mis1nput@users.noreply.github.com>
2025-12-09 18:53:37 -0600

    Update create-users-script.sh

--- create-users-script.sh
+++ create-users-script.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
+##Allows for Functions to be easily accesed##
+TARGET_USER=""
 
 echo "
-##############################
-##      Creating Users      ## 
-##############################
+ _____                _   _               _   _                   
+/  __ \              | | (_)             | | | |                  
+| /  \/_ __ ___  __ _| |_ _ _ __   __ _  | | | |___  ___ _ __ ___ 
+| |   | '__/ _ \/ _` | __| | '_ \ / _` | | | | / __|/ _ \ '__/ __|
+| \__/\ | |  __/ (_| | |_| | | | | (_| | | |_| \__ \  __/ |  \__ \
+ \____/_|  \___|\__,_|\__|_|_| |_|\__, |  \___/|___/\___|_|  |___/
+                                   __/ |                          
+                                  |___/                           
+
 "
 
 echo "
+################################
 ## Creating User Velociraptor ##
+################################
 "
+## Sets User for Search
+TARGET_USER="velociraptor"
+##Checking For the User##
+if user_exists "$TARGET_USER"; then
+  echo "User exists skipping user creation"
+else 
+    sudo useradd "velociraptor"
+    ## User Created ##
+    read -p "User Created, Password Prompting is Next"
+    sudo passwd velociraptor
+    echo "Password Created!!"
+    getent passwd | grep velociraptor
 
-sudo useradd "velociraptor"
-## User Created ##
-echo "Please Enter a Password for User"
-sudo passwd velociraptor
-echo "Password Created!!"
-getent passwd | grep velociraptor
 
+
+##Functions
+user_exists() {
+    getent passwd "$1" >/dev/null 2>&1
+  }
 
