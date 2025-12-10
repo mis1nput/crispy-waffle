1fbffed028fbcd08238c9ba7e13d5adda89ad7c6
mis1nput <161495409+mis1nput@users.noreply.github.com>
2025-12-09 18:33:43 -0600

    Update create-users-script.sh

--- create-users-script.sh
+++ create-users-script.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-echo {"
+
 ##############################
 ##      Creating Users      ## 
 ##############################
-"}
 
-echo {"
+
 ## Creating User Velociraptor ##
-"}
 sudo useradd "velociraptor"
-echo "User Created!!"
+## User Created ##
 echo "Please Enter a Password for User"
 sudo passwd velociraptor
 echo "Password Created!!"
