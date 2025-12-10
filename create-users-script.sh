d338dad334b5decba6b155c27527443dcf3d8ceb
mis1nput <161495409+mis1nput@users.noreply.github.com>
2025-12-09 19:18:55 -0600

    Refactor user creation logic in script
    Refactor user creation script to use a loop and selection function for user names.

--- create-users-script.sh
+++ create-users-script.sh
@@ -5,41 +5,49 @@ user_exists() {
     getent passwd "$1" >/dev/null 2>&1
   }
 
+user_name_selection {
+    if $USER_COUNT="0"
+        $TARGET_USER="velociraptor"
+    if $USER_COUNT="1"
+        $TARGET_USER="tim"
+    if $USER_COUNT="2"
+        $TARGET_USER="lex"
+    if $USER_COUNT="3"
+        $TARGET_USER="dr.ian-malcolm"
+    if $USER_COUNT="4"
+        $TARGET_USER="the-lawyer"
+}
+
 
 ##Allows for Functions to be easily accesed##
 TARGET_USER=""
+USER_COUNT="0"
 
-echo "
- _____                _   _               _   _                   
-/  __ \              | | (_)             | | | |                  
-| /  \/_ __ ___  __ _| |_ _ _ __   __ _  | | | |___  ___ _ __ ___ 
-| |   | '__/ _ \/ _` | __| | '_ \ / _` | | | | / __|/ _ \ '__/ __|
-| \__/\ | |  __/ (_| | |_| | | | | (_| | | |_| \__ \  __/ |  \__ \
- \____/_|  \___|\__,_|\__|_|_| |_|\__, |  \___/|___/\___|_|  |___/
-                                   __/ |                          
-                                  |___/                           
+while [$USER_COUNT != 5]; do
 
-"
-
-echo "
-################################
-## Creating User Velociraptor ##
-################################
-"
-## Sets User for Search
-TARGET_USER="velociraptor"
-##Checking For the User##
-if user_exists "$TARGET_USER"; then
-  echo "User exists skipping user creation"
-else 
-  sudo useradd "velociraptor"
-  ## User Created ##
-  read -p "User Created, Password Prompting is Next"
-  sudo passwd velociraptor
-  echo "Password Created!!"
-  getent passwd | grep velociraptor
-fi
-
+    echo "
+    ################################
+    ##       Creating Users       ##
+    ################################
+    "
+    ## Sets User for Search
+    user_name_selection
+    ##Checking For the User##
+    echo "Checking if Exists"
+    if user_exists "$TARGET_USER"; then
+      echo ""$TARGET_USER" exists skipping user creation"
+      $USER_COUNT+1
+    else 
+      echo "$TARGET_USER doesn't exist begin creating user...."
+      sudo useradd "$TARGET_USER"
+      ## User Created ##
+      read -p "$TARGET_USER Created, Password Prompting is Next"
+      sudo passwd $TARGET_USER
+      echo "Password Created!!"
+      getent passwd | grep $TARGET_USER
+      $USER_COUNT+1
+    fi
+done
 
 
 
