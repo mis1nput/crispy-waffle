2325ce448a3660ef3f6ecef1f2c4f2017e5efe0f
mis1nput <161495409+mis1nput@users.noreply.github.com>
2025-12-11 14:02:53 -0600

    Update and rename create-folders to create-folders.sh

--- create-folders.sh
+++ create-folders.sh
@@ -1,3 +1,4 @@
+
 #!/Bin/Bash
 
 folder-check-creation(){
@@ -14,8 +15,8 @@ folder-check-creation(){
 change-dir(){
   case "$DIR-COUNT" in
   0) $DIR-PATH="/isla-nublar" ;;
-  1) $DIR-PATH="$DIR-PATH+/visitor-center" ;;
-  2) $DIR-PATH="$DIR-PATH+/kitchen" ;;
+  1) $DIR-PATH="/isla-nublar/visitor-center" ;;
+  2) $DIR-PATH="/isla-nublar/visitor-center/kitchen" ;;
   esac
 } 
 
@@ -26,7 +27,22 @@ create-files(){
 }
 
 give-permissions(){
+  $DIR-COUNT="1"
+  change-dir
+  chmod 777 $DIR-PATH
+  $DIR-COUNT="2"
+  change-dir
+  chmod 700 $DIR-PATH
+  setfacl -m u:velociraptor:rwx $DIR-PATH
+  setfacl -m u:tim:rwx $DIR-PATH
+  setfacl -m u:lex:rwx $DIR-PATH
+  chmod 700 ice-cream.txt
+  chmod 700 jello.txt
+  setfacl -m u:tim:rwx ice-cream.txt
+  setfacl -m u:lex:rwx jello.txt
+}
 
 
 DIR-PATH = ""
 DIR-COUNT = ""
+
