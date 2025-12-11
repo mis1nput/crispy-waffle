a7741e2e04a74a8a711afbe899e315de723989e5
mis1nput <161495409+mis1nput@users.noreply.github.com>
2025-12-11 14:15:36 -0600

    Update create-folders.sh

--- create-folders.sh
+++ create-folders.sh
@@ -1,48 +1,54 @@
 
-#!/Bin/Bash
+#!/bin/bash
 
-folder-check-creation(){
-  if [ -d "$DIR-PATH" ]; then
-      echo '$DIR-PATH' Exists Skipping
+folder_check_creation(){
+  if [ -d "$DIR_PATH" ]; then
+      echo "'$DIR_PATH' Exists Skipping"
   else
-      echo "'$DIR-PATH' Doesn't Exist, Starting Creation"
-      mkdir $DIR-PATH
-      ls $DIR-PATH
+      echo "'$DIR_PATH' Doesn't Exist, Starting Creation"
+      mkdir -p "$DIR_PATH"
+      ls "$DIR_PATH"
   fi
-  DIR-COUNT=$((DIR-COUNT+1))
+  DIR_COUNT=$((DIR_COUNT+1))
 }
 
-change-dir(){
-  case "$DIR-COUNT" in
-  0) $DIR-PATH="/isla-nublar" ;;
-  1) $DIR-PATH="/isla-nublar/visitor-center" ;;
-  2) $DIR-PATH="/isla-nublar/visitor-center/kitchen" ;;
+change_dir(){
+  case "$DIR_COUNT" in
+  0) DIR_PATH="/isla-nublar" ;;
+  1) DIR_PATH="/isla-nublar/visitor-center" ;;
+  2) DIR_PATH="/isla-nublar/visitor-center/kitchen" ;;
   esac
 } 
 
-create-files(){
-  cd $DIR-PATH
+create_files(){
+  cd "$DIR_PATH"
   touch ice-cream.txt
   touch jello.txt
 }
 
-give-permissions(){
-  $DIR-COUNT="1"
-  change-dir
-  chmod 777 $DIR-PATH
-  $DIR-COUNT="2"
-  change-dir
-  chmod 700 $DIR-PATH
-  setfacl -m u:velociraptor:rwx $DIR-PATH
-  setfacl -m u:tim:rwx $DIR-PATH
-  setfacl -m u:lex:rwx $DIR-PATH
+give_permissions(){
+  DIR_COUNT=1
+  change_dir
+  chmod 777 "$DIR_PATH"
+  DIR_COUNT=2
+  change_dir
+  chmod 700 "$DIR_PATH"
+  setfacl -m u:velociraptor:rwx "$DIR_PATH"
+  setfacl -m u:tim:rwx "$DIR_PATH"
+  setfacl -m u:lex:rwx "$DIR_PATH"
   chmod 700 ice-cream.txt
   chmod 700 jello.txt
-  setfacl -m u:tim:rwx ice-cream.txt
-  setfacl -m u:lex:rwx jello.txt
+  setfacl -m u:tim:rwx "$DIR_PATH/ice-cream.txt"
+  setfacl -m u:lex:rwx "$DIR_PATH/jello.txt"
 }
 
 
-DIR-PATH = ""
-DIR-COUNT = ""
+DIR_PATH=""
+DIR_COUNT=0
 
+while [ "$DIR_COUNT" -lt 3 ]; do
+  change_dir
+  folder_check_creation
+done
+create_files
+give_permissions
